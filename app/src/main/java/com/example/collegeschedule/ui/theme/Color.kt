b84f2decc412bd33c7068c809d0662316113de09
Saryuna <s.syrenova.07@mail.ru>
2026-02-18 18:34:21 +0700

    Добавлена цветовая схема с кремовым фоном (#FDFBD7) и коричневым текстом (#815E35)

--- app/src/main/java/com/example/collegeschedule/ui/theme/Color.kt
+++ app/src/main/java/com/example/collegeschedule/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.example.collegeschedule.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val CreamBackground = Color(0xFFFDFBD7)
+val BrownPrimary = Color(0xFF815E35)
+
+
+val BrownDark = Color(0xFF5C4224)
+val BrownLight = Color(0xFFA67C52)
+
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray = Color(0xFF9E9E9E)
