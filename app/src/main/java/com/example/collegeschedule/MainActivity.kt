086644c351d0bde3a1d5459eacdaf593e71b7c67
Saryuna <s.syrenova.07@mail.ru>
2026-02-18 18:06:58 +0700

    Добавлена инициализация RetrofitInstance при запуске приложения

--- app/src/main/java/com/example/collegeschedule/MainActivity.kt
+++ app/src/main/java/com/example/collegeschedule/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import com.example.collegeschedule.data.network.RetrofitInstance
 import com.example.collegeschedule.ui.schedule.ScheduleScreen
 import com.example.collegeschedule.ui.theme.CollegeScheduleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        RetrofitInstance.initialize(this)
+
         enableEdgeToEdge()
         setContent {
             CollegeScheduleTheme {
