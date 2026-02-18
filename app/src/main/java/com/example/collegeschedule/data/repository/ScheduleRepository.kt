7c8936eeb3e238ca46936ca33e493f763494b6e7
Saryuna <s.syrenova.07@mail.ru>
2026-02-18 18:05:22 +0700

    Добавлены методы для сохранения и загрузки избранной группы

--- app/src/main/java/com/example/collegeschedule/data/repository/ScheduleRepository.kt
+++ app/src/main/java/com/example/collegeschedule/data/repository/ScheduleRepository.kt
@@ -1,9 +1,16 @@
 package com.example.collegeschedule.data.repository
 
 import com.example.collegeschedule.data.api.ScheduleApi
+import com.example.collegeschedule.data.dto.GroupDto
 import com.example.collegeschedule.data.dto.ScheduleByDateDto
+import com.example.collegeschedule.data.local.FavoritesManager
+import kotlinx.coroutines.flow.Flow
 
-class ScheduleRepository(private val api: ScheduleApi) {
+class ScheduleRepository(
+    private val api: ScheduleApi,
+    private val favoritesManager: FavoritesManager
+) {
+
     suspend fun loadSchedule(
         group: String,
         start: String = "2026-02-09",
@@ -14,5 +21,19 @@ class ScheduleRepository(private val api: ScheduleApi) {
             start = start,
             end = end
         )
+    }
+
+    suspend fun loadGroups(): List<GroupDto> {
+        return api.getGroups()
+    }
+
+    val selectedGroupFlow: Flow<String?> = favoritesManager.selectedGroupFlow
+
+    suspend fun saveSelectedGroup(groupName: String) {
+        favoritesManager.saveSelectedGroup(groupName)
+    }
+
+    suspend fun clearSelectedGroup() {
+        favoritesManager.clearSelectedGroup()
     }
 }
